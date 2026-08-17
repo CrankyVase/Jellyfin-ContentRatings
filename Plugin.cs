3bc64c728c2ee029ae564c69cef40f88ef6c6611
CrankyVase <lewis.farina@hmstudents.org>
2026-08-17 17:48:10 -0500

    Fix: Use correct namespaces from MediaBrowser.Common.Plugins and MediaBrowser.Common.Configuration

--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,7 @@
 using Jellyfin.Plugin.ContentRatings.Configuration;
 using Jellyfin.Plugin.ContentRatings.Services;
-using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 using Microsoft.Extensions.DependencyInjection;
