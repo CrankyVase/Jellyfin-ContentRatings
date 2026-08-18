dbf2f24d1444473cb53b7971cc773098c7b65591
CrankyVase <lewis.farina@hmstudents.org>
2026-08-18 08:47:16 -0500

    Add IPluginServiceRegistrator to register services

---
+++ ServiceRegistrator.cs
@@ -0,0 +1,22 @@
+using Jellyfin.Plugin.ContentRatings.Services;
+using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Controller;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jellyfin.Plugin.ContentRatings;
+
+public class ServiceRegistrator : IPluginServiceRegistrator
+{
+    public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
+    {
+        serviceCollection.AddHttpClient<IWikidataService, WikidataService>(client =>
+        {
+            client.BaseAddress = new Uri("https://www.wikidata.org/w/api.php");
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("Jellyfin.Plugin.ContentRatings/1.0");
+            client.Timeout = TimeSpan.FromSeconds(30);
+        });
+
+        serviceCollection.AddSingleton<ICacheService, CacheService>();
+        serviceCollection.AddSingleton<IContentRatingsProvider, ContentRatingsProvider>();
+    }
+}
